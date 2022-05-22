0f5e80f443d468fb2965905ce16de14d8756e460
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-22 09:51:24 +0600

    comlete slider section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div >
      <Navbar></Navbar>
+     <Home></Home>
     </div>
   );
 }
---
+++ src/pages/Home/Slider.js
@@ -0,0 +1,49 @@
+import React from "react";
+import slider1 from "../../assets/img/slider1.jpg";
+import slide2 from "../../assets/img/slide2.png";
+import bg1 from "../../assets/img/bg1.jpg";
+import image1 from "../../assets/img/image1.jpg";
+import image2 from "../../assets/img/image2.jpg";
+import image3 from "../../assets/img/image3.jpg";
+const Slider = () => {
+  return (
+    <>
+      <div
+        style={{ backgroundImage: `url(${slider1})` }}
+        className="h-[95vh] relative w-screen"
+      >
+        <div className="flex lg:flex-row flex-col justify-around items-center">
+          <div className="lg:mt-0 mt-14">
+            <h1 className="text-xl font-bold text-white">Products</h1>
+            <h1 className="lg:text-9xl text-5xl font-bold ml-0 text-white">
+              DPB-600
+            </h1>
+            <h1 className="lg:text-xl text-sm font-bold mt-3 text-white">
+              BETTER BLOWER QUIENTLY CLEARS WAY
+            </h1>
+          </div>
+          <div>
+            <img className="mt-8 h-[80%]" src={slide2} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{ backgroundImage: `url(${bg1})` }}
+        className="h-[70vh] w-screen lg:block hidden"
+      >
+        <div className="absolute mt-[-150px] lg:ml-40">
+          <div className="flex lg:flex-row flex-col justify-center h-[90%]   items-center">
+            <img className=" lg:h-[500px]" src={image1} alt="" />
+            <div className="">
+              <img className=" lg:h-[250px]" src={image2} alt="" />
+              <img className=" lg:h-[250px]" src={image3} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Slider;
