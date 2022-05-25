da722979757f47eb74be6872a58bf54a9301c2fd
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-25 08:12:16 +0600

    landing make admin page

--- src/pages/Dashboard/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../components/Loading';
 
 const MakeAdmin = () => {
+    const { data:users, isLoading, refetch } = useQuery('users', () =>fetch('http://localhost:5000/users').then(res =>res.json()))
+    if(isLoading){
+        return <Loading></Loading>
+    }
     return (
-        <div>
-            <h1>from make admin page</h1>
+        <div className="px-20 pt-10">
+        <div className="overflow-x-auto">
+          <h1 className="text-center mb-5 text-3xl font-bold  uppercase">
+            All  user
+          </h1>
+          <table className="table w-full">
+            {/* <!-- head --> */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Make Admin</th>
+                <th>delete</th>
+                <th>payment</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <!-- row 1 --> */}
+              {users?.map((user, index) => (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{user.email}</td>
+                  <td>
+                  <button
+                     
+                     className="btn btn-xs bg-yellow-700"
+                   >
+                     admin
+                   </button></td>
+                  <td>
+                    <button
+                     
+                      className="btn btn-xs bg-red-700"
+                    >
+                      delete
+                    </button>
+                  </td>
+                  <td>
+                    <button  className="btn btn-xs bg-amber-700">
+                      payment
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     );
 };
 
